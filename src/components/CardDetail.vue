3e667c59ccf252604f863873b5ffd40b1264eb20
Michael Wales <michaelwales@gmail.com>
2020-04-25 14:35:50 +0100

    Adds remote images

--- src/components/CardDetail.vue
+++ src/components/CardDetail.vue
@@ -1,6 +1,13 @@
 <template lang="html">
   <div>
-    <h3>{{card.name}}</h3>
+    <h2>{{card.name}}</h2>
+    <img width="200px" :src="getImageURL">
+    <br>
+    <i>"{{card.flavor}}"</i>
+    <h3>Class:</h3>
+      <p>{{card.playerClass}}</p>
+    <h3></h3>
+
   </div>
 </template>
 
@@ -8,7 +15,14 @@
 
 export default {
   name: 'card-detail',
-  props: ['card']
+  props: ['card'],
+  computed: {
+    getImageURL() {
+      const cardImage = this.card.cardId;
+      const cardURL = 'https://art.hearthstonejson.com/v1/render/latest/enUS/256x/' + cardImage + '.png';
+      return cardURL;
+    }
+  }
 }
 </script>
 

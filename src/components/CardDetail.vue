b4b378c29890eb6dd536654a9609df47bd5bc3d9
Michael Wales <michaelwales@gmail.com>
2020-04-26 13:42:00 +0100

    Adds all card details

--- src/components/CardDetail.vue
+++ src/components/CardDetail.vue
@@ -2,11 +2,44 @@
   <div>
     <h2>{{card.name}}</h2>
     <img width="200px" :src="getImageURL">
+
     <br>
-    <i>"{{card.flavor}}"</i>
+
+    <i v-if="card.flavor">"{{card.flavor}}"</i>
+
     <h3>Class:</h3>
       <p>{{card.playerClass}}</p>
-    <h3></h3>
+
+    <h3>Card Set:</h3>
+      <p>{{card.cardSet}}</p>
+
+    <h3>Card Rarity:</h3>
+      <p v-if="card.rarity">{{card.rarity}}</p>
+      <p v-else>Common</p>
+
+    <h3>Race:</h3>
+      <p v-if="card.race">{{card.race}}</p>
+      <p v-else>N/A</p>
+
+    <h3>Type:</h3>
+      <p v-if="card.type">{{card.type}}</p>
+      <p v-else>N/A</p>
+
+    <h3>Mana Cost:</h3>
+      <p v-if="card.cost">{{card.cost}}</p>
+      <p v-else>N/A</p>
+
+    <h3>Health:</h3>
+      <p v-if="card.health">{{card.health}}</p>
+      <p v-else>N/A</p>
+
+    <h3>Attack:</h3>
+      <p v-if="card.attack">{{card.attack}}</p>
+      <p v-else>N/A</p>
+
+    <h3>Abilities:</h3>
+      <i v-if="card.text">"{{card.text}}"</i>
+      <p v-else>None</p>
 
   </div>
 </template>
